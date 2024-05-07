606e4c90f012e9fc36deec7d727527e590723d06
Jon Holtzman <holtz@nmsu.edu>
2024-05-06 22:12:19 -0600

    update qhy_watchdog

--- qhy_watchdog.py
+++ qhy_watchdog.py
@@ -1,7 +1,7 @@
 import time
 from alpaca.camera import *
 import usbrelay
-from yoctopuce 
+
 from yoctopuce.yocto_api import *
 from yoctopuce.yocto_temperature import *
 
@@ -9,29 +9,34 @@ errmsg=YRefParam()
 YAPI.RegisterHub("usb",errmsg)
 
 # open camera and relay
-C=Camera('localhost:11111',0)
+C=Camera('172.24.4.202:11111',0)
+C.CoolerOn = True
+C.SetCCDTemperature=-10
 relay=usbrelay.USBRelay()
-thermocouple=YTemperature.FindTemperature("THRMCPL1-286EEC.temperature1")
-thermocouple.isOnline()
+thermo1=YTemperature.FindTemperature("THRMCPL1-286EEC.temperature1")
+thermo1.isOnline()
+thermo2=YTemperature.FindTemperature("THRMCPL1-286EEC.temperature2")
+
 
 # check every minute
-while True :
+for i in range(10) :
     # if CCDtemp < 30, reset watchdog with relay
     # if watchdog is not reset, set it to cut power in xxx minutes
-    try:
+    if True:
         tccd = C.CCDTemperature
-        thermocouple.get_currentValue()
-        print('tccd: {:f}  thermocouple: {:f}', tccd,thermocouple)
-        if temp < 30 :
+        t1 =thermo1.get_currentValue()
+        t2 =thermo2.get_currentValue()
+        print('tccd: {:f} {:f} {:f} thermocouple: {:f} {:f}'.format(tccd,C.SetCCDTemperature,C.CoolerPower,t1,t2))
+        if t1 < 30 :
             # reset watchdog
             print('resetting watchdog...')
             relay.on_relay(1)
             time.sleep(1)
-            relay.on_relay(1)
+            relay.off_relay(1)
         else :
             print('NOT resetting watchdog...')
-    except:
-        pass
+    #except:
+    #    pass
 
     print('sleeping...')
     time.sleep(10)
