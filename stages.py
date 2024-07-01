aec4a8ab228208dc9103d39481a5188362beb6b6
Jon Holtzman <holtz@nmsu.edu>
2024-07-01 14:28:52 -0600

    add initial stages

---
+++ stages.py
@@ -0,0 +1,27 @@
+import lts
+import zaber
+
+HOST = "10.75.0.202"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+def remote() :
+  """ Run simple remote socket server 
+  """
+  lts=lts.ThorlabsStage()
+  zaber=zaber.ZaberStage()
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                print('done')
+                break
+            print('received: ', data)
+            #conn.sendall(data)
+
+
+
