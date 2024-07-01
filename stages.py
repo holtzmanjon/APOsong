b741ff82839590f160d2cc46dac51a998027c83e
Jon Holtzman <holtz@nmsu.edu>
2024-07-01 14:56:02 -0600

    initial test implement commands

--- stages.py
+++ stages.py
@@ -1,14 +1,16 @@
 import lts
 import zaber
+imnport socket
 
-HOST = "10.75.0.22"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "10.75.0.202"  # Standard loopback interface address (localhost)
+HOST = "172.24.4.202"  # Standard loopback interface address (localhost)
+PORT = 76543  # Port to listen on (non-privileged ports are > 1023)
 
 def remote() :
   """ Run simple remote socket server 
   """
-  lts=lts.ThorlabsStage()
-  zaber=zaber.ZaberStage()
+  s1=lts.ThorlabsStage()
+  s2=zaber.ZaberStage()
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -22,6 +24,14 @@ def remote() :
                 break
             print('received: ', data)
             #conn.sendall(data)
+            cmd = data.decode()
+            if cmd == 'iodine 1' :
+                s1.move(1000)
+            elif cmd == 'iodine 0' :
+                s1.move(0)
+            elif cmd.split()[0] == 'focus' :
+                s2.move(int(cmd.split()[1]))
+
 
 
 
