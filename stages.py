ae7eb7529241e19dd27f3879739c2c7f463aedee
Jon Holtzman <holtz@nmsu.edu>
2024-07-11 16:44:35 -0600

    update for tc, etc.

--- stages.py
+++ stages.py
@@ -1,6 +1,7 @@
 import lts
 import zaber
 import socket
+import serial
 
 HOST = "10.75.0.202"  # Standard loopback interface address (localhost)
 HOST = "172.24.4.202"  # Standard loopback interface address (localhost)
@@ -11,6 +12,8 @@ def remote() :
   """
   s1=lts.ThorlabsStage()
   s2=zaber.ZaberStage()
+  tc=Serial('COM3',115200,timeout=1)
+
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -24,11 +27,24 @@ def remote() :
                 break
             print('received: ', data)
             #conn.sendall(data)
-            cmd = data.decode()
-            if cmd == 'iodine 1' :
-                s1.move(1000)
-            elif cmd == 'iodine 0' :
-                s1.move(0)
-            elif cmd.split()[0] == 'focus' :
-                s2.move(int(cmd.split()[1]))
+            input = data.decode()
+            cmd = input.split()[0]
+            try: val = input.split()[1]
+            except : val = ''
+            if cmd == 'iodine_pos' :
+                if len(val) > 0 :
+                    s1.move(int(val))
+                return s1.get_position()
+            elif cmd == 'focus' :
+                if len(val) > 0 :
+                    s2.move(int(val))
+                return s2.get_position()
+            elif cmd == 'iodine_temp' :
+                if len(val) > 0 :
+                    tc.write(b'TSET1=140\r')
+                    tc.readline()
+                    tc.write(b'TSET2=140\r')
+                    tc.readline()
+                tc.write(b'TACT1?\r')
+                return tc.readline()
 
