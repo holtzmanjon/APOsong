99d4746194773a0183c456e693a17dc2bcdcbd87
Jon Holtzman <holtz@nmsu.edu>
2024-07-12 13:23:57 -0600

    put in infinite loop

--- stages.py
+++ stages.py
@@ -16,9 +16,10 @@ def remote() :
 
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
+    while True :
+      s.listen()
+      conn, addr = s.accept()
+      with conn:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
