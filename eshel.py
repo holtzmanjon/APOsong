6e61c1e5dbbb9764c1ab347360b7ecb7c1e61e57
Jon Holtzman <holtz@nmsu.edu>
2024-09-30 12:48:19 -0600

    mod to avoid ending when remote connection is closed

--- eshel.py
+++ eshel.py
@@ -56,9 +56,10 @@ def remote() :
   relaycard.clear(9)
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
+    while True :
+      s.listen()
+      conn, addr = s.accept()
+      with conn:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
