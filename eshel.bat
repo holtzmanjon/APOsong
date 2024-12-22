5968af34f2a79c9ab73cbaafcd20d2ec772c257d
Jon Holtzman <holtz@nmsu.edu>
2024-12-21 19:45:44 -0700

    add eshel.bat

---
+++ eshel.bat
@@ -0,0 +1,3 @@
+call \Users\SONG\anaconda3\condabin\activate.bat
+cd \Users\SONG\APOsong
+python eshel.py
--- eshel.py
+++ eshel.py
@@ -213,6 +213,7 @@ def main(argv):
         sleep(1)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+#    main(sys.argv[1:])
+    remote()
 
 
