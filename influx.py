a3372370d926ff26cb73c72e9e3acd4992102c45
Jon Holtzman <holtz@nmsu.edu>
2025-04-11 15:07:03 -0500

    new database token

--- influx.py
+++ influx.py
@@ -4,6 +4,10 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import pdb
 
 os.environ['INFLUX_TOKEN'] = 'v-RuHY6T1pyOIL1SU9lrWYKYEU_SDZ0VWkPHOIU9hMECF7axu2wiFzY1u8N7J6s9KCbOreQKI43mJUi9pj5BbA=='
+
+os.environ['INFLUX_TOKEN'] = 'unP6W2cm9uLUWIi9jVj8tDjhYsN286L3NWcmuNFN8tvJOh9Pewgc9LQLPYx302XAebNhHIVnv0ubi365V0oUPA=='
+
+
 # Store the URL of your InfluxDB instance
 url="http://localhost:8086"
 token = os.environ['INFLUX_TOKEN']
