25f2cd6e9cd4779ef528df0948099efcf3792f6c
Jon Holtzman <holtz@nmsu.edu>
2025-09-18 21:06:22 -0600

    update to database

--- weather.py
+++ weather.py
@@ -1,8 +1,10 @@
 from socket import *
 import time
 from datetime import datetime
+from astropy.time import Time
 from astropy.table import Table
 import influx
+import database
 
 def getapo() :
     """ Get APO weather status from network query
@@ -26,19 +28,26 @@ def getapo() :
     return ldict
 
 
-def database() :
+def postgres_write(wdict) :
     """ Loads subset of weather quantities into table for postgres db loading
     """
-    wdict=getapo()
     tab=Table()
-    tab['timestamp'] = [datetime.now()]
+    #tab['timestamp'] = [datetime.now()]
+    tab['dateobs'] = [Time.now().fits]
     tab['temp'] = float(wdict['airtemp'])
     tab['hum'] = float(wdict['humidity'])
     tab['pressure'] = float(wdict['pressure'])
     tab['dewpoint'] = float(wdict['dewpoint'])
     tab['wind_speed'] = float(wdict['winds'])
     tab['wind_dir'] = float(wdict['windd'])
+    if wdict['encl35m'] == 'open' : tab['encl35m'] = 1
+    else : tab['encl35m'] = 0
+    if wdict['encl25m'] == 'open' : tab['encl25m'] = 1
+    else : tab['encl25m'] = 0
 
+    d=database.DBSession(host='localhost',database='db_apo')
+    d.ingest('public.weather',tab,onconflict='update')
+    d.close()
     return tab
 
 def influx_write(wdict) :
