8188532c85ce9670bbeb3fd6dd5cc24ee41ccc93
Jon Holtzman <holtz@nmsu.edu>
2025-11-05 19:32:54 -0700

    add initial docs for reduce

--- reduce.py
+++ reduce.py
@@ -13,6 +13,31 @@ from holtztools import plots
 
 def specreduce(n, red=None, trace=None, wav=None, retrace=False, cr=True, scat=False, response=None, write=False, display=None, clobber=False) :
     """ Quick reduction
+
+    Parameters
+    ----------
+    n : str or int
+        number or name of file to reduce
+    red : Reducer object, default=None
+        pyvista imred.Reducer object for doing basic reduction
+    trace : Trace object, default=None
+        pyvista spectra.Trace object for extraction
+    wav : WaveCal object, default=None
+        pyvista spectra.WaveCal object for wavelength calibration
+    retrace : bool, default=False
+        set to True to retrace on object
+    cr : bool, default=True
+        set to True to do CR cleaning
+    scat : bool, default=False
+        set to True to do scattered light correction
+    response :  pyvista Data object, default=None
+        response function to load into output
+    write : bool, default=False
+        set to True to write extracted frame to disk
+    display : pyvista TV object, default=None
+        if given, display data during reduction, and pause for inspection
+    clobber : bool, default=False
+        if True, re-reduce objects even if output file exists already
     """
 
     if red == None :
@@ -55,6 +80,25 @@ def specreduce(n, red=None, trace=None, wav=None, retrace=False, cr=True, scat=F
     return imec
 
 def plot(ax,i,name,mag=None,song=None,red=None,orders=[34],write=False):
+    """ Reduce and add plot of extracted spectrum, throughput, and S/N for an image to existing Axes
+
+    Parameters
+    ==========
+    ax : Axes[3]
+        matplotlib Axes to plot into
+    i : int or str
+        number or name of file to reduce
+    name : str
+        name for plot legend
+    mag : float, default=None
+        magnitude of object to compute throughput, if not given, query database
+    red : pyvista Reducer, default=None
+        Reducer for reducing object
+    orders : list, default=[34]
+        Order(s) to plot
+    write : bool, default=False
+        set to True to write out extracted data
+    """
     spec=specreduce(i,red=red,write=write)
     targ=spec.header['OBJECT'].split('.')[0].replace('_iodine','')
     if mag == None :
@@ -82,114 +126,30 @@ def plot(ax,i,name,mag=None,song=None,red=None,orders=[34],write=False):
     sn=np.max(spec.data.flatten()[j1:j2]/spec.uncertainty.array.flatten()[j1:j2])
     return t,sn
 
-def mjd(ut='UT251003',rn=None, write=False) :
-    red=imred.Reducer('SONG',dir='/data/1m/'+ut)
-    if rn is not None : red.rn = [rn]
-    fig,ax=plots.multi(1,3,figsize=(8,4),hspace=0.001)
-    if ut == 'UT251003' :
-        plot(ax,61,'muHer_a',song=25000/120,red=red,write=write)
-        plot(ax,62,'muHer_b',red=red,write=write)
-        plot(ax,63,'muHer_i_a',red=red,write=write)
-        plot(ax,64,'muHer_i_b',red=red,write=write)
-        plot(ax,74,'HD185144_a',red=red,song=17500/300,write=write)
-        plot(ax,75,'HD185144_b',red=red,write=write)
-        plot(ax,76,'HD185144_i_a',red=red,write=write)
-        plot(ax,77,'HD185144_i_b',red=red,write=write)
-        plot(ax,78,'gammaCep_a',red=red,song=35000/120,write=write)
-        plot(ax,79,'gammaCep_b',red=red,write=write)
-        plot(ax,80,'gammaCep_i_a',red=red,write=write)
-        plot(ax,81,'gammaCep_i_b',red=red,write=write)
-    elif ut == 'UT251006' :
-        plot(ax,113,'gammaCep_a',red=red,write=write)
-        plot(ax,114,'gammaCep_b',red=red,write=write)
-        plot(ax,115,'gammaCep_i_a',red=red,write=write)
-        plot(ax,116,'gammaCep_i_b',red=red,write=write)
-        plot(ax,117,'HD185144_a',red=red,write=write)
-        plot(ax,118,'HD185144_b',red=red,write=write)
-    elif ut == 'UT251011' :
-        plot(ax,64,'HR7235_a',red=red,write=write)
-        plot(ax,65,'HR7235_b',red=red,write=write)
-        plot(ax,66,'HR7235_i_a',red=red,write=write)
-        plot(ax,67,'HR7235_i_b',red=red,write=write)
-        plot(ax,83,'HR6410_a',red=red,write=write)
-        plot(ax,84,'HR6410_b',red=red,write=write)
-        plot(ax,85,'HR6410_i_a',red=red,write=write)
-        plot(ax,86,'HR6410_i_b',red=red,write=write)
-        plot(ax,86,'HR6410_i_b',red=red,write=write)
-        plot(ax,117,'HR264_a',red=red,write=write)
-        plot(ax,118,'HR264_b',red=red,write=write)
-        plot(ax,119,'HR264_i_a',red=red,write=write)
-        plot(ax,120,'HR264_i_b',red=red,write=write)
-        plot(ax,232,'HD10780_a',red=red,write=write)
-        plot(ax,233,'HD10780_b',red=red,write=write)
-        plot(ax,234,'HD10780_i_a',red=red,write=write)
-        plot(ax,235,'HD10780_i_b',red=red,write=write)
-        plot(ax,266,'gammaCep_a',red=red,write=write)
-        plot(ax,267,'gammaCep_b',red=red,write=write)
-        plot(ax,268,'gammaCep_i_a',red=red,write=write)
-        plot(ax,269,'gammaCep_i_b',red=red,write=write)
-    elif ut == 'UT251016' :
-        plot(ax,224,'HR2845_a',red=red,write=write)
-        plot(ax,225,'HR2845_b',red=red,write=write)
-        plot(ax,226,'HR2845_i_a',red=red,write=write)
-        plot(ax,227,'HR2845_i_b',red=red,write=write)
-    elif ut == 'UT251017' :
-        for targ,num in zip(['HR7235','HR6410','HR264'],[46,59,72]) :
-            for i in range(4) :
-              if i== 0 : ext = '_a'
-              elif i== 1 : ext = '_b'
-              elif i== 2 : ext = '_i_a'
-              elif i== 3 : ext = '_i_b'
-              plot(ax,num+i,targ+ext,red=red,write=write)
-    elif ut == 'UT251025' :
-        for targ,num in zip(['HD185144','HR264','HR1087','HR2845','HR3494'],[89,126,148,209,222]) :
-            for i in range(4) :
-              if i== 0 : ext = '_a'
-              elif i== 1 : ext = '_b'
-              elif i== 2 : ext = '_i_a'
-              elif i== 3 : ext = '_i_b'
-              plot(ax,num+i,targ+ext,red=red,write=write)
-        for targ,num in zip(['HD23249','HD23249','HD23249','HD23249'],[161,173,185,197]) :
-            for i in range(3) :
-              if i== 0 : ext = '_a'
-              elif i== 1 : ext = '_b'
-              elif i== 2 : ext = '_c'
-              plot(ax,num+i,targ+ext,red=red,write=write)
-
-    d=database.DBSession() 
-    targs=d.query('robotic.target')
-    d.close()
-    for targ in ['muHer','HD185144','gamCep'] :
-        j=np.where(targs['targname'] == targ)[0]
-        mag=targs['mag'][j][0]
-        if targ == 'muHer' : song = 25000/120
-        elif targ == 'HD185144' : song = 17500/300
-        elif targ == 'gammaCep' or targ == 'gamCep' : song = 35000/120
-        ax[0].scatter([5564],[song*10**(0.4*mag)],s=50)
-    for i in range(3) :
-        lim = ax[i].get_ylim()
-        ax[i].set_ylim(0,lim[1])
-    ax[0].legend(fontsize='xx-small',ncol=3)
-
-
-    fig.suptitle(ut)
-    fig.savefig(ut+'.png')
-    #plt.close()
-    return fig,ax
-
 def throughput() :
+    """ Make plot of throughput from database query
+    """
 
     d=database.DBSession()
     out=d.query(sql='select * from obs.reduced as red join obs.exposure as exp on red.exp_pk = exp.exp_pk')
+    targs=d.query('robotic.target')
     d.close()
     i=np.where(out['filter'] == 'iodine')
     o=np.where(out['filter'] != 'iodine')
-    plt.figure(figsize=(8,4))
-    plt.scatter(out['mjd'][i],out['throughput'][i],c=out['alt'][i],marker='+',label='no iodine')
-    plt.scatter(out['mjd'][o],out['throughput'][o],c=out['alt'][o],marker='s',label='iodine')
-    plt.ylim(0,6000)
-    plt.colorbar()
-    xlim=plt.xlim()
+    mag=[]
+    for f in out['file'] :
+        targ = os.path.basename(f).split('.')[0]
+        j=np.where(targs['targname'] == targ)[0][0]
+        mag.append(targs[j]['mag'])
+    mag=np.array(mag)
+    fig,ax=plots.multi(1,2,figsize=(10,8),hspace=0.001)
+    ax[0].scatter(out['mjd'][i],out['throughput'][i],c=out['alt'][i],marker='+',label='iodine')
+    scat=ax[0].scatter(out['mjd'][o],out['throughput'][o],c=out['alt'][o],marker='s',label='no iodine')
+    plt.colorbar(scat)
+    ax[1].scatter(out['mjd'][i],out['throughput'][i],c=mag[i],marker='+',label='iodine')
+    scat=ax[1].scatter(out['mjd'][o],out['throughput'][o],c=mag[o],marker='s',label='no iodine')
+    plt.colorbar(scat)
+    xlim=ax[0].get_xlim()
     for targ in ['muHer','HD185144','gammaCep'] :
         if targ == 'muHer' :
             song = 25000/120
@@ -200,15 +160,20 @@ def throughput() :
         elif targ == 'gammaCep' or targ == 'gamCep' :
             mag=3.22
             song = 35000/120
-        plt.plot(xlim,[song*10**(0.4*mag),song*10**(0.4*mag)],label='Tenerife {:s}'.format(targ))
+        ax[0].plot(xlim,[song*10**(0.4*mag),song*10**(0.4*mag)],label='Tenerife {:s}'.format(targ))
 
-    plt.legend()
-    plt.xlabel('MJD')
-    plt.ylabel('cnts/s scaled to m=0')
-    plt.title('Throughput (color coded by altitude)')
+    ax[0].set_ylim(0,6000)
+    ax[1].set_ylim(0,6000)
+    ax[0].legend()
+    ax[1].set_xlabel('MJD')
+    ax[0].set_ylabel('cnts/s scaled to m=0')
+    ax[0].set_title('Throughput at 5560-70 (color coded by altitude)')
+    ax[1].set_title('Throughput at 5560-70 (color coded by mag)')
+    fig.tight_layout()
 
 def guider(i1,i2,red=None,sat=65000,title='') :
-
+    """ Make plots of derived star positions from guider sequence
+    """
     fig,ax=plots.multi(2,2)
     for i in range(i1,i2) :
         a=red.rd(i)
