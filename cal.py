7c67662b9c523b6f3a19146c035c33e687f0348e
Jon Holtzman <holtz@nmsu.edu>
2025-11-19 16:43:24 -0700

    add sfocrun()

--- cal.py
+++ cal.py
@@ -1,6 +1,10 @@
 # routines for obtaining calibration data using eShel calibration source
 import aposong
 import time
+from holtztools import plots
+from pyvista import imred, spectra
+import matplotlib.pyplot as plt
+import pdb
 
 def getlamps() :
     state = ['Off','On']
@@ -72,7 +76,7 @@ def dark(exptime,ccdtemp=None,cam=0,n=5,bin=1,filt=None,name=None) :
     for i in range(n) :
         aposong.expose(exptime,cam=cam,light=False,bin=bin,filt=filt,name=name)
 
-def darks(temps=[-15,-10,-5],bins=[1],exps=[0,90,120,900],cam=0,n=11,filt=None) :
+def darks(temps=[-10,-5,-15],bins=[2],exps=[30,60,120,180,240,300,600],cam=3,n=11,filt=None) :
     """ Take multiple series of darks at different temps, bins, exptimes
     """
     for temp in temps :
@@ -82,4 +86,52 @@ def darks(temps=[-15,-10,-5],bins=[1],exps=[0,90,120,900],cam=0,n=11,filt=None) 
             for exp in exps :
                name='dark_cam{:d}_temp{:d}_bin{:d}_t{:d}'.format(cam,temp,bin,exp)
                dark(exp,cam=cam,n=n,filt=filt,bin=bin,name=name)
+
+
+def sfocrun(foc0=427500,red=None,trace=None,wav=None,display=None,plotonly=False) :
+
+    if red == None : red=imred.Reducer('SONG',dir='/data/1m/UT251119')
+    if trace == None : trace=spectra.Trace('./UT251119_Trace_fiber2.fits')
+    if wav == None : wav=spectra.WaveCal('./UT251119_WaveCal_fiber2.fits')
+
+    if not plotonly :
+        aposong.specfoc(foc0)
+
+        aposong.calstage_find()
+        aposong.calstage_in()
+        lamps(quartz=True,led=True)
+        s=aposong.sexp(50,name='flat',display=display,max=65535)
+        t=red.reduce(s.name)
+        trace.retrace(t)
+
+        lamps(thar=True)
+
+    fiber='specfoc'
+    for i,df in enumerate(range(-2500,2501,500)) :
+        if not plotonly :
+            aposong.specfoc(foc0+df)
+            s=aposong.sexp(50,name='{:s}_{:d}'.format(fiber,foc0+df),display=display,max=65535)
+            t=red.reduce(s.name)
+        else :
+            t=red.reduce(267+i)
+            if display is not None : 
+                display.tv(t)
+                display.imexam()
+        tec=trace.extract(t)
+        wav=spectra.WaveCal('./UT251119_WaveCal_fiber2.fits')
+        wav.identify(tec)
+        fig,ax=plots.multi(1,2,hspace=0.001)
+        plots.plotc(ax[0],wav.waves,wav.fwhm,wav.pix,yr=[0,10],yt='FWHM',colorbar=True,zt='xpixel',cmap='viridis',size=50)
+        ax[0].set_ylim(0,10)
+        plots.plotc(ax[1],wav.waves,wav.waves/(wav.fwhm*(wav.wave(pixels=[wav.pix,wav.y])-wav.wave(pixels=[wav.pix+1,wav.y]))),wav.pix,
+                   colorbar=True,yr=[60000,100000],xt='Wavelength',yt='R',zt='xpixel',size=50,cmap='viridis')
+        ax[0].grid()
+        ax[1].grid()
+        fig.suptitle('specfoc: {:d}'.format(foc0+df))
+        fig.savefig('{:s}_{:d}.png'.format(fiber,foc0+df))
+        plt.close()
+    if not plotonly: 
+        lamps()
+        aposong.calstage_out()
+        aposong.specfoc(foc0)
 
